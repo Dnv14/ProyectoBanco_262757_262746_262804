c0bb3a3978ca44813cdc5dcc14d6028da9b0657c
Dnv14 <diego.navarro.140706@gmail.com>
2026-02-17 16:42:17 -0700

    proyectoPrueba
    ola

---
+++ ProyectoBanco_262757_262746_262804/src/main/java/com/mycompany/proyectobanco_262757_262746_262804/ProyectoBanco_262757_262746_262804.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectobanco_262757_262746_262804;
+
+/**
+ *
+ * @author Diego
+ */
+public class ProyectoBanco_262757_262746_262804 {
+
+    public static void main(String[] args) {
+        System.out.println("El diego es crack!");
+    }
+}
