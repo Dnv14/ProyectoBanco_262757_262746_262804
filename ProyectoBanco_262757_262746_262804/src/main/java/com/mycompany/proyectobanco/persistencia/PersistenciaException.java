d70cd5d8c996db513905cea750f36ab48c585aa1
juliangracia16-crypto <juliangracia16@outlook.com>
2026-02-17 18:11:08 -0700

    Create PersistenciaException.java

---
+++ ProyectoBanco_262757_262746_262804/src/main/java/com/mycompany/proyectobanco/persistencia/PersistenciaException.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.proyectobanco.persistencia;
+
+/**
+ *
+ * @author Julian
+ */
+public class PersistenciaException extends Exception{
+
+    public PersistenciaException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    
+}
