1d72d85f97c9b98d699f65092b18b4d7e6448b90
juliangracia16-crypto <juliangracia16@outlook.com>
2026-02-17 18:09:07 -0700

    Entidad Cuenta
    Cuenta con constructores, getters y setters

---
+++ ProyectoBanco_262757_262746_262804/src/main/java/com/mycompany/proyectobanco/entidades/Cuenta.java
@@ -0,0 +1,74 @@
+
+package com.mycompany.proyectobanco.entidades;
+
+import java.util.GregorianCalendar;
+
+/**
+ *
+ * @author Julian
+ */
+public class Cuenta {
+    
+    public enum Estado{
+        ACTIVO,INACTIVO
+    }
+    
+    private String numeroCuenta;
+    private Estado estado;
+    private GregorianCalendar fechaApertura;
+    private Integer saldo;
+    private String idCliente;
+
+    public Cuenta() {}
+    
+       
+    public Cuenta(String numeroCuenta, Estado estado, GregorianCalendar fechaApertura, Integer saldo, String idCliente) {
+        this.numeroCuenta = numeroCuenta;
+        this.estado = estado;
+        this.fechaApertura = fechaApertura;
+        this.saldo = saldo;
+        this.idCliente = idCliente;
+    }
+
+    public String getNumeroCuenta() {
+        return numeroCuenta;
+    }
+
+    public Estado getEstado() {
+        return estado;
+    }
+
+    public GregorianCalendar getFechaApertura() {
+        return fechaApertura;
+    }
+
+    public Integer getSaldo() {
+        return saldo;
+    }
+
+    public String getIdCliente() {
+        return idCliente;
+    }
+
+    public void setNumeroCuenta(String numeroCuenta) {
+        this.numeroCuenta = numeroCuenta;
+    }
+
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
+
+    public void setFechaApertura(GregorianCalendar fechaApertura) {
+        this.fechaApertura = fechaApertura;
+    }
+
+    public void setSaldo(Integer saldo) {
+        this.saldo = saldo;
+    }
+
+    public void setIdCliente(String idCliente) {
+        this.idCliente = idCliente;
+    }
+    
+    
+}
