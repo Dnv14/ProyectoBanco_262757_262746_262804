55c95de2da3c1fca4cb48031ec0cd84abd4e33fb
HunterHenry67 <enrique.madrid262804@potros.itson.edu.mx>
2026-02-18 21:23:36 -0700

    TransferenciaPrueba

--- ProyectoBanco_262757_262746_262804/src/main/java/com/mycompany/proyectobanco_262757_262746_262804/ProyectoBanco_262757_262746_262804.java
+++ ProyectoBanco_262757_262746_262804/src/main/java/com/mycompany/proyectobanco_262757_262746_262804/ProyectoBanco_262757_262746_262804.java
@@ -5,10 +5,14 @@
 package com.mycompany.proyectobanco_262757_262746_262804;
 
 import com.mycompany.proyectobanco.dtos.NuevaOperacionDTO;
+import com.mycompany.proyectobanco.dtos.NuevaTransferenciaDTO;
 import com.mycompany.proyectobanco.entidades.Operacion;
+import com.mycompany.proyectobanco.negocio.ITransferenciaBO;
 import com.mycompany.proyectobanco.persistencia.IOperacionDAO;
+import com.mycompany.proyectobanco.persistencia.ITransferenciaDAO;
 import com.mycompany.proyectobanco.persistencia.OperacionDAO;
 import com.mycompany.proyectobanco.persistencia.PersistenciaException;
+import com.mycompany.proyectobanco.persistencia.TransferenciaDAO;
 import java.time.LocalDateTime;
 
 /**
@@ -18,13 +22,21 @@ import java.time.LocalDateTime;
 public class ProyectoBanco_262757_262746_262804 {
 
     public static void main(String[] args) {
-        LocalDateTime fechaHora = LocalDateTime.now();
-        NuevaOperacionDTO nuevaOperacionDTO = new NuevaOperacionDTO(500, fechaHora ,"1234567890123456");
-        IOperacionDAO operacion = new OperacionDAO();
+//        LocalDateTime fechaHora = LocalDateTime.now();
+//        NuevaOperacionDTO nuevaOperacionDTO = new NuevaOperacionDTO(500, fechaHora ,"1234567890123456");
+//        IOperacionDAO operacion = new OperacionDAO();
+//        try{
+//            operacion.realizarOperacion(nuevaOperacionDTO);
+//        }catch(PersistenciaException ex){
+//            System.out.println("Error");
+//        }   
+        
+        NuevaTransferenciaDTO nuevaTransfDTO = new NuevaTransferenciaDTO(1, "9876543210987654");
+        ITransferenciaDAO nuevaTransferenciaDAO = new TransferenciaDAO();
         try{
-            operacion.realizarOperacion(nuevaOperacionDTO);
+            nuevaTransferenciaDAO.crearTransferencia(nuevaTransfDTO);
         }catch(PersistenciaException ex){
-            System.out.println("Error");
-        }   
+            System.out.println("error");
+        }
     }
 }
