7b2617a6a5bc2c1be5e588d116be07e112cee9d9
Dnv14 <diego.navarro.140706@gmail.com>
2026-02-18 19:25:15 -0700

    CORRECIONES

--- ProyectoBanco_262757_262746_262804/src/main/java/com/mycompany/proyectobanco/persistencia/OperacionDAO.java
+++ ProyectoBanco_262757_262746_262804/src/main/java/com/mycompany/proyectobanco/persistencia/OperacionDAO.java
@@ -9,47 +9,48 @@ import com.mycompany.proyectobanco.entidades.Operacion;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Timestamp;
+import java.sql.Statement;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.GregorianCalendar;
 import java.util.logging.Logger;
 
 /**
  *
  * @author BALAMRUSH
  */
-public class OperacionDAO implements IOperacionDAO{
+public class OperacionDAO implements IOperacionDAO {
 
     private static final Logger LOGGER = Logger.getLogger(OperacionDAO.class.getName());
 
     @Override
     public Operacion realizarOperacion(NuevaOperacionDTO nuevaOpreacionDTO) throws PersistenciaException {
-        try{
+        try {
             String codigoSQL = """
                                INSERT INTO Operacion(monto, fechaHora, tipoOperacion, numeroCuenta)
                                values(?,?,?,?);
                                """;
             Connection conexion = ConexionBD.crearConexion();
-            PreparedStatement comando = conexion.prepareStatement(codigoSQL);
+            PreparedStatement insertarOperacionStmt = conexion.prepareStatement(codigoSQL,Statement.RETURN_GENERATED_KEYS
+            );
+
             LocalDateTime fechaHora = LocalDateTime.now();
             DateTimeFormatter formatterFechaHora = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
             String formatoFechaHora = fechaHora.format(formatterFechaHora);
-            
+
             comando.setInt(1, nuevaOpreacionDTO.getMonto());
             comando.setString(2, formatoFechaHora);
             comando.setString(3, nuevaOpreacionDTO.getTipoOperacion());
             comando.setString(4, nuevaOpreacionDTO.getNumeroCuenta());
-            
-            boolean resultado = comando.execute();
+
+            comando.execute();
             LOGGER.fine("Se registró la operación");
-            
+
             return new Operacion(0, nuevaOpreacionDTO.getMonto(), nuevaOpreacionDTO.getFechaHora(), nuevaOpreacionDTO.getTipoOperacion(), nuevaOpreacionDTO.getNumeroCuenta());
-            
+
         } catch (SQLException ex) {
             LOGGER.severe(ex.getMessage());
-            throw new PersistenciaException("No fue posible registrar la operación", ex);         
+            throw new PersistenciaException("No fue posible registrar la operación", ex);
         }
     }
-    
+
 }
