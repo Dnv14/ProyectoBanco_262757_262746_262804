f9da071e7d349548fc7234b09b86ccfbc174e8a6
juliangracia16-crypto <juliangracia16@outlook.com>
2026-02-22 17:02:16 -0700

    avances login y menu principal

--- ProyectoBanco_262757_262746_262804/src/main/java/com/mycompany/proyectobanco/presentacion/MenuPrincipalFORM.java
+++ ProyectoBanco_262757_262746_262804/src/main/java/com/mycompany/proyectobanco/presentacion/MenuPrincipalFORM.java
@@ -32,6 +32,8 @@ public class MenuPrincipalFORM extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
+        jPanel1.setBackground(new java.awt.Color(153, 153, 153));
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
